20570f520977bfd5d01c528aa316aced29d72fbf
Scott Spencer <scottspencer003@yahoo.com>
2015-10-11 21:25:12 -0400

    cool jquery stuff

---
+++ js/script.js
@@ -0,0 +1,107 @@
+	$(document).ready(function(){
+
+			$("#socialMedia").delay(1000).animate({
+				left:"+=64px"
+			}, 250);
+
+			$('#content').fadeIn(2000);
+
+
+			$("#hide").click(function(){
+        		$("#navBar").hide();
+   		 	});
+    		$("#show").click(function(){
+        		$("#navBar").show();
+   			 });
+
+    		$(".icon1").hover(function() {
+    			$(this).animate({
+    				height: '70px',
+    				width: '70px',
+    			});
+    			
+    		},
+    		function() {
+    			$(this).animate({
+    				height: '64px',
+    				width: '64px',
+    			});
+    		});
+
+    		$(".icon2").hover(function() {
+    			$(this).animate({
+    				height: '70px',
+    				width: '70px',
+    			});
+    			
+    		},
+    		function() {
+    			$(this).animate({
+    				height: '64px',
+    				width: '64px',
+    			});
+    		});
+
+    		$(".icon3").hover(function() {
+    			$(this).animate({
+    				height: '70px',
+    				width: '70px',
+    			});
+    			
+    		},
+    		function() {
+    			$(this).animate({
+    				height: '64px',
+    				width: '64px',
+    			});
+    		});
+
+    		$(".icon4").hover(function() {
+    			$(this).animate({
+    				height: '70px',
+    				width: '70px',
+    			});
+    			
+    		},
+    		function() {
+    			$(this).animate({
+    				height: '64px',
+    				width: '64px',
+    			});
+    		});
+
+    		$(".icon5").hover(function() {
+    			$(this).animate({
+    				height: '70px',
+    				width: '70px',
+    			});
+    			
+    		},
+    		function() {
+    			$(this).animate({
+    				height: '64px',
+    				width: '64px',
+    			});
+    		});
+
+
+		});
+
+
+
+
+
+
+
+
+
+
+
+// 	$(document).ready(function(){
+//     $("#hide").click(function(){
+//         $("p").hide();
+//     });
+//     $("#show").click(function(){
+//         $("p").show();
+//     });
+// });
